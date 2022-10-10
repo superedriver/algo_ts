cff85397f8e154e3763b3382d782646d826bcd47
Super E Driver <superedriver@gmail.com>
2022-10-10 20:30:13 +0300

    Added Linear

---
+++ src/search/linear.ts
@@ -0,0 +1,17 @@
+export function linearSearch(arr: number[], item: number): number {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === item) return i;
+    }
+    return -1
+}
+
+// avoid n "i < arr.length" check
+export function sentinelLinearSearch(arr: number[], item: number): number {
+    arr[arr.length] = item;
+    let i = 0;
+    while(arr[i] !== item) {
+        i += 1;
+    }
+
+    return i === arr.length - 1 ? -1 : i;
+}
---
+++ tests/search/linear.test.ts
@@ -0,0 +1,41 @@
+import { linearSearch, sentinelLinearSearch } from "../../src/search/linear"
+
+describe('Search', () => {
+    describe('Linear', () => {
+        test('Contains value', () => {
+            expect(linearSearch([0,1,2,3,4,5], 0)).toBe(0);
+            expect(linearSearch([0,1,2,3,4,5], 1)).toBe(1);
+            expect(linearSearch([0,1,2,3,4,5], 2)).toBe(2);
+            expect(linearSearch([0,1,2,3,4,5], 3)).toBe(3);
+            expect(linearSearch([0,1,2,3,4,5], 4)).toBe(4);
+            expect(linearSearch([0,1,2,3,4,5], 5)).toBe(5);
+        });
+
+        test('Does Not Contain value', () => {
+            expect(linearSearch([0,1,2,3,4,5], 8)).toBe(-1);
+        });
+
+        test('Empty Array', () => {
+            expect(linearSearch([], 8)).toBe(-1);
+        });
+    });
+
+    describe('Sentinel Linear', () => {
+        test('Contains value', () => {
+            expect(sentinelLinearSearch([0,1,2,3,4,5], 0)).toBe(0);
+            expect(sentinelLinearSearch([0,1,2,3,4,5], 1)).toBe(1);
+            expect(sentinelLinearSearch([0,1,2,3,4,5], 2)).toBe(2);
+            expect(sentinelLinearSearch([0,1,2,3,4,5], 3)).toBe(3);
+            expect(sentinelLinearSearch([0,1,2,3,4,5], 4)).toBe(4);
+            expect(sentinelLinearSearch([0,1,2,3,4,5], 5)).toBe(5);
+        });
+
+        test('Does Not Contain value', () => {
+            expect(sentinelLinearSearch([0,1,2,3,4,5], 8)).toBe(-1);
+        });
+
+        test('Empty Array', () => {
+            expect(sentinelLinearSearch([], 8)).toBe(-1);
+        });
+    });
+});
