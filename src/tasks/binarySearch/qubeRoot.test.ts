3dc2db379df5830f04e7775d20d1e2ad38a78a2d
superedriver <superedriver@gmail.com>
2025-09-29 17:47:17 +0200

    Added qubeRoot max precision

--- src/tasks/binarySearch/qubeRoot.test.ts
+++ src/tasks/binarySearch/qubeRoot.test.ts
@@ -1,9 +1,25 @@
-import { qubeRoot } from "./qubeRoot";
+import {
+  qubeRoot,
+  qubeRootMaxPrecisionCycle,
+  qubeRootMaxPrecision,
+} from "./qubeRoot";
 
 describe("qubeRoot", () => {
   test("qubeRoot", () => {
     expect(qubeRoot(27)).toBe(3.000244140625);
     expect(qubeRoot(64)).toBe(4.000152587890625);
     expect(qubeRoot(1000)).toBe(10.000656127929688);
+  });
+
+  test("qubeRootMaxPrecisionCycle", () => {
+    expect(qubeRootMaxPrecisionCycle(27)).toBe(3);
+    expect(qubeRootMaxPrecisionCycle(64)).toBe(4);
+    expect(qubeRootMaxPrecisionCycle(1000)).toBe(10);
+  });
+
+  test("qubeRootMaxPrecision", () => {
+    expect(qubeRootMaxPrecision(27)).toBe(3);
+    expect(qubeRootMaxPrecision(64)).toBe(4);
+    expect(qubeRootMaxPrecision(1000)).toBe(10);
   });
 });
--- src/tasks/binarySearch/qubeRoot.ts
+++ src/tasks/binarySearch/qubeRoot.ts
@@ -1,5 +1,5 @@
 // a > 0
-export const qubeRoot = (a: number) => {
+export const qubeRoot = (a: number): number => {
   let bad = -1;
   let good = a + 1;
 
@@ -11,6 +11,42 @@ export const qubeRoot = (a: number) => {
     } else {
       bad = m;
     }
+  }
+
+  return good;
+};
+
+const MAX_DIVIDE_OPERATIONS = 100;
+export const qubeRootMaxPrecisionCycle = (a: number): number => {
+  let bad = -1;
+  let good = a + 1;
+
+  for (let i = 0; i < MAX_DIVIDE_OPERATIONS; i++) {
+    const m = (good + bad) / 2;
+
+    if (m * m * m >= a) {
+      good = m;
+    } else {
+      bad = m;
+    }
+  }
+
+  return good;
+};
+
+export const qubeRootMaxPrecision = (a: number): number => {
+  let bad = -1;
+  let good = a + 1;
+
+  let m = (good + bad) / 2;
+  while (m !== good && m !== bad) {
+    if (m * m * m >= a) {
+      good = m;
+    } else {
+      bad = m;
+    }
+
+    m = (good + bad) / 2;
   }
 
   return good;
